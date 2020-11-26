8463c167232b9720ae8cca6fe7336267103cb10d
dg <danigaglia89@gmail.com>
2020-11-26 01:12:06 +0100

    cors fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: process.env.CORS_ORIGIN_ALLOWED || "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
